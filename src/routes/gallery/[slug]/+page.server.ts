7ff4b07786d6a473ac7e69f181a1a0ffe8a3a877
Jo Nykrem <jo.nykrem@mf.me>
2026-01-31 09:59:03 +0100

    delete +page.ts in gallery/[slug]

--- src/routes/gallery/[slug]/+page.server.ts
+++ src/routes/gallery/[slug]/+page.server.ts
@@ -1,5 +1,12 @@
-import { getArtworkBySlug } from "$lib/utils/content.js";
+import { getAllArtworks, getArtworkBySlug } from "$lib/utils/content";
 import { error } from "@sveltejs/kit";
+
+export const prerender = true;
+
+export const entries = async () => {
+    const artworks = await getAllArtworks();
+    return artworks.map((a) => ({ slug: a.slug }));
+};
 
 export async function load({ params }) {
     const artwork = await getArtworkBySlug(params.slug);
--- src/routes/gallery/[slug]/+page.ts
+++
@@ -1,8 +0,0 @@
-import { getAllArtworks } from "$lib/utils/content";
-
-export const prerender = true;
-
-export const entries = async () => {
-    const artworks = await getAllArtworks();
-    return artworks.map((a) => ({ slug: a.slug }));
-};
