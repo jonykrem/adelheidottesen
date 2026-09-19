6739205fe9fd5fe37ea922f424142f8b0c8ee772
Jo Nykrem <jo.nykrem@mf.me>
2026-09-19 11:49:06 +0200

    Read Sanity project ID from environment

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const sanity = createClient({
-    projectId: "vaix14a0",
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID || "vaix14a0",
     dataset: import.meta.env.VITE_SANITY_DATASET || "production",
     apiVersion: "2024-01-01",
     useCdn: !import.meta.env.DEV,
