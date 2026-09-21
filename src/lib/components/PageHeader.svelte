3666a0bd69076fdddf9a4de685da6b88001d0f1c
Jo Nykrem <jo.nykrem@mf.me>
2026-09-21 20:14:34 +0200

    style: refine page header spacing

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -1,46 +1,59 @@
 <script lang="ts">
     export let title: string | undefined = undefined;
+    export let showActions = false;
 </script>
 
-<header class="page-header">
+<header class="page-header" class:has-actions={showActions}>
     {#if title}
         <h2 class="page-title">{title}</h2>
     {/if}
-    <div class="page-actions">
-        <slot />
-    </div>
+    {#if showActions}
+        <div class="page-actions">
+            <slot />
+        </div>
+    {/if}
 </header>
 
 <style>
     .page-header {
         display: flex;
         align-items: baseline;
-        justify-content: flex-end;
+        justify-content: space-between;
+        margin-bottom: 0;
+    }
+
+    .page-header.has-actions {
         margin-bottom: 2rem;
     }
 
     @media (max-width: 767px) {
         .page-header {
-            margin-top: -1.75rem;
-            margin-bottom: 1rem;
+            margin-top: -0.75rem;
+        }
+
+        .page-header.has-actions {
+            margin-bottom: 2rem;
         }
     }
 
     .page-title {
+        flex: 1;
         font-weight: 400;
         margin: 0;
+        text-align: left;
     }
 
     .page-actions {
         display: flex;
         align-items: baseline;
+        justify-content: flex-end;
         gap: 0.75rem;
+        margin-left: auto;
     }
 
     @media (max-width: 767px) {
         .page-actions {
             justify-content: flex-end;
-            width: 100%;
             margin-top: 0.5rem;
         }
     }
